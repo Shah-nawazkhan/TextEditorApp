14aa664e7007f7f81b5af058edb55c8f0e45f2d1
Shahnawaz <shahnawazkhan333@live.com>
2024-03-15 14:44:30 +0500

    Added my project

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"; 
+import { Text, Platform, KeyboardAvoidingView, SafeAreaView, ScrollView} 
+	from "react-native"; 
+import { actions, RichEditor, RichToolbar } 
+	from "react-native-pell-rich-editor"; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Custom component for heading1 action 
+const handleHead = ({ tintColor }) => ( 
+	<Text style={{ color: tintColor }}>H1</Text> 
+); 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => { 
+
+	// Create a reference to the 
+	// RichEditor component 
+	const richText = React.useRef(); 
+	return ( 
+		<SafeAreaView> 
+			<ScrollView> 
+
+				{/* KeyboardAvoidingView to 
+					handle keyboard behavior */} 
+				<KeyboardAvoidingView 
+					behavior={ 
+						Platform.OS ==="ios"? "padding": "height"} 
+					style={{ flex: 1 }}> 
+
+					{/* Text component for 
+						the app title */} 
+					<Text style={{ color: "green", 
+								fontSize: 30, 
+								marginTop: 50, 
+								backgroundColor:"lightgrey", 
+								borderRadius: 10, 
+								textAlign: "center",}}> 
+					 Text Editor App
+					</Text> 
+
+					{/* RichToolbar component 
+					for text formatting actions */} 
+					<RichToolbar style={{ marginTop: 10}} 
+								editor={richText} 
+
+						// Connect the RichToolbar 
+						// to the RichEditor 
+						actions={[ actions.setBold, 
+								actions.insertBulletsList, 
+								actions.insertOrderedList, 
+								actions.insertLink, 
+								actions.setStrikethrough, 
+								actions.setItalic, 
+								actions.setUnderline, 
+								actions.heading1,]} 
+
+						// Define available text 
+						// formatting actions 
+						iconMap={{[actions.heading1]:handleHead,}} /> 
+
+					{/* Text component for 
+						description */} 
+					<Text style={{ fontFamily: "monospace", 
+								fontWeight: 900, 
+								fontSize: 15, 
+								padding: 10}}> 
+						Description: 
+					</Text> 
+
+					{/* RichEditor component 
+						for text editing */} 
+					<RichEditor ref={richText} 
+								onChange={(descriptionText) => { 
+
+							// Handle the change in 
+							// the editor's content 
+							console.log("descriptionText:",descriptionText);}} /> 
+				</KeyboardAvoidingView> 
+			</ScrollView> 
+		</SafeAreaView> 
+	); 
+}; 
+export default App;
